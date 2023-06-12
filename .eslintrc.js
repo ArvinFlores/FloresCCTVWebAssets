63b1ab6ad726f5d802add80d8b34c513eb206b4d
Arvin Flores <arvinflores255@gmail.com>
2023-06-12 10:51:30 -0700

    Added linting support

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "@typescript-eslint/semi": "off",
+    "semi": [2, "always"],
+    "max-len": [2, { "code": 120 }]
+  },
+  settings: {
+    react: { version: 'detect' }
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+import type { FunctionComponent } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => <h1>FloresCCTV app</h1>;
+const App: FunctionComponent = () => {
+  return <h1>FloresCCTV app</h1>;
+};
 
-const container = document.getElementById('app');
+const container = document.getElementById('app') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);

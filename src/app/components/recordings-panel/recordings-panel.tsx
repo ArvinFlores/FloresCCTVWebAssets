36de9d4f1050c5a903b13ba7b2549290ab169bfd
Arvin <arvin.flores@metalab.csun.edu>
2023-11-27 18:18:28 -0800

    add health check panel to RecordingsPanel component

--- src/app/components/recordings-panel/recordings-panel.tsx
+++ src/app/components/recordings-panel/recordings-panel.tsx
@@ -1,7 +1,10 @@
 import './recordings-panel.css';
 
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faWrench } from '@fortawesome/free-solid-svg-icons/faWrench';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
 import type { FileStorage } from 'florescctvwebservice-types';
 import { Button } from 'src/components/button';
 import { OutsideClick } from 'src/components/outside-click';
@@ -15,6 +18,7 @@ import {
   daysBetween,
   daysAgoFormat
 } from 'src/util/datetime';
+import { classnames } from 'src/util/classnames';
 import { RecordingItem } from './components/recording-item';
 import type { RecordingsPanelProps } from './interfaces';
 import { createStickyHeaderItems } from './helpers';
@@ -23,6 +27,8 @@ export function RecordingsPanel ({
   onClose,
   onItemClick
 }: RecordingsPanelProps): JSX.Element {
+  const initialPanel = 'recordings';
+  const [activePanel, setActivePanel] = useState<'recordings' | 'health'>(initialPanel);
   const {
     data,
     hasFetched,
@@ -72,79 +78,128 @@ export function RecordingsPanel ({
         className="recordings-panel__content"
         onClick={onClose}
       >
-        <div className="util-flex-container util-flex-container--h-sb">
-          <div />
-          <Button
-            ariaLabel="Close recordings panel"
-            className="util-mr-2 util-mt-2 recordings-panel__menu-btn"
-            variant="see-through"
-            circular={true}
-            onClick={onClose}
-          >
-            <FontAwesomeIcon
-              icon={faBars}
-              size="2x"
-            />
-          </Button>
+        <div
+          className={classnames({
+            'recordings-panel__header': true,
+            'util-flex-container': true,
+            'util-flex-container--h-left': activePanel !== initialPanel,
+            'util-flex-container--h-right': activePanel === initialPanel,
+            'util-mr-2': true,
+            'util-ml-2': true,
+            'util-mt-2': true
+          })}
+        >
+          {
+            activePanel === initialPanel ?
+              (
+                <>
+                  <Button
+                    ariaLabel="Open camera health panel"
+                    variant="see-through"
+                    circular={true}
+                    onClick={() => {
+                      setActivePanel('health');
+                    }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faWrench}
+                      size="2x"
+                    />
+                  </Button>
+                  <Button
+                    ariaLabel="Close recordings panel"
+                    className="recordings-panel__menu-btn"
+                    variant="see-through"
+                    circular={true}
+                    onClick={onClose}
+                  >
+                    <FontAwesomeIcon
+                      icon={faBars}
+                      size="2x"
+                    />
+                  </Button>
+                </>
+              ) :
+              (
+                <Button
+                  ariaLabel="Go back to main panel"
+                  variant="see-through"
+                  circular={true}
+                  onClick={() => {
+                    setActivePanel('recordings');
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faArrowLeft}
+                    size="2x"
+                  />
+                </Button>
+              )
+          }
         </div>
         {
-          hasFetched ?
-            (
-              data?.files && data.files.length > 0 ?
-                (
-                  <StickyHeaderList
-                    height="calc(100% - 75px)"
-                    items={createStickyHeaderItems(data.files).map(({ header, children }) => {
-                      const itemDate = new Date(header);
-                      const currYear = new Date().getFullYear();
-                      const itemYear = itemDate.getFullYear();
-                      const longFormat = itemYear === currYear ? 'mm dd' : 'mm dd, yyyy';
-                      const dayDiff = daysBetween(new Date(), itemDate);
+          activePanel === 'recordings' && (
+            hasFetched ?
+              (
+                data?.files && data.files.length > 0 ?
+                  (
+                    <StickyHeaderList
+                      height="calc(100% - 75px)"
+                      items={createStickyHeaderItems(data.files).map(({ header, children }) => {
+                        const itemDate = new Date(header);
+                        const currYear = new Date().getFullYear();
+                        const itemYear = itemDate.getFullYear();
+                        const longFormat = itemYear === currYear ? 'mm dd' : 'mm dd, yyyy';
+                        const dayDiff = daysBetween(new Date(), itemDate);
 
-                      return {
-                        header: dayDiff > 6 ? dateFormat(itemDate, longFormat) : daysAgoFormat(itemDate, true),
-                        children: children.map((item) => {
-                          const { id, thumbnail, created_at: createdAt } = item;
+                        return {
+                          header: dayDiff > 6 ? dateFormat(itemDate, longFormat) : daysAgoFormat(itemDate, true),
+                          children: children.map((item) => {
+                            const { id, thumbnail, created_at: createdAt } = item;
 
-                          return (
-                            <div
-                              key={id}
-                              onClick={onItemClick.bind(null, item)}
-                            >
-                              <RecordingItem
-                                thumbnail={thumbnail}
-                                label={ampmFormat(new Date(createdAt))}
-                              />
+                            return (
+                              <div
+                                key={id}
+                                onClick={onItemClick.bind(null, item)}
+                              >
+                                <RecordingItem
+                                  thumbnail={thumbnail}
+                                  label={ampmFormat(new Date(createdAt))}
+                                />
+                              </div>
+                            );
+                          })
+                        };
+                      })}
+                      postContent={
+                        data.nextPageToken != null ?
+                          (
+                            <div className="util-mb-3 util-mt-3 util-ta-c">
+                              {status !== 'error' && <Spinner size="small" />}
+                              {showError}
                             </div>
-                          );
-                        })
-                      };
-                    })}
-                    postContent={
-                      data.nextPageToken != null ?
-                        (
-                          <div className="util-mb-3 util-mt-3 util-ta-c">
-                            {status !== 'error' && <Spinner size="small" />}
-                            {showError}
-                          </div>
-                        ) :
-                        null
-                    }
-                    onEndReached={data.nextPageToken != null ? handleOnEndScroll : undefined}
-                  />
-                ) :
-                (
-                  <div className="util-perfect-center">
-                    <p>The camera has not recorded anything yet</p>
-                  </div>
-                )
-            ) :
-            (
-              <div className="util-perfect-center util-ta-c">
-                {status === 'loading' && <Spinner size="small" />}
-                {showError}
-              </div>
-            )
+                          ) :
+                          null
+                      }
+                      onEndReached={data.nextPageToken != null ? handleOnEndScroll : undefined}
+                    />
+                  ) :
+                  (
+                    <div className="util-perfect-center">
+                      <p>The camera has not recorded anything yet</p>
+                    </div>
+                  )
+              ) :
+              (
+                <div className="util-perfect-center util-ta-c">
+                  {status === 'loading' && <Spinner size="small" />}
+                  {showError}
+                </div>
+              )
+          )
+        }
+        {
+          activePanel === 'health' && <div />
         }
       </OutsideClick>
     </div>
